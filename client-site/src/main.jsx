23c042bccd1e6bdfbce599f533d0d99690aea130
tahfeezmizan <mizanahmed5316@gmail.com>
2024-09-05 21:08:29 +0600

    setup react route, google font, tailwind css, create basic folder structure

--- client-site/src/main.jsx
+++ client-site/src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+import Root from './Root/Root';
+import Home from './pages/Home/Home';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      }
+    ]
+  },
+
+]);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ client-site/src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            <p className="text-5xl font-InterBarlow">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae expedita ducimus ipsa? Pariatur, impedit cumque? Repudiandae deserunt autem culpa quis, nostrum cum doloribus eligendi? Id, fugit. Fugit ipsa iste placeat!</p>
+        </div>
+    );
+};
+
+export default Home;
