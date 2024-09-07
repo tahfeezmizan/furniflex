06dd4b6af78dea4141b301cf253f4d6dc9ee21a4
tahfeezmizan <mizanahmed5316@gmail.com>
2024-09-07 12:25:56 +0600

    bone category query

--- server-site/index.js
+++ server-site/index.js
@@ -8,7 +8,6 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 app.use(cors())
 app.use(express.json())
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7utjicv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -22,27 +21,58 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
-        // await client.connect();
+        // Connect the client to the server (this establishes the connection)
+        await client.connect();
+
+        const productCollection = client.db('FurniFlexDB').collection('products');
+
+        app.get('/products', async (req, res) => {
+            try {
+                const page = parseInt(req.query.page) || 0;
+                const limit = parseInt(req.query.limit) || 6;
+                const category = req.query.category || '';
+
+                const skip = page * limit;
+
+                let query = {};
+                if (category) {
+                    query = { category: category }; 
+                }
+
+                const result = await productCollection.find(query)
+                    .skip(skip)
+                    .limit(limit)
+                    .toArray();
+
+                res.send(result);
+            } catch (error) {
+                console.error("Failed to load products data:", error);
+                res.status(500).send({
+                    message: "Failed to load products data.",
+                    error: error.message || "Internal Server Error"
+                });
+            }
+        });
 
 
-
+        app.get('/productsCount', async (req, res) => {
+            const count = await productCollection.estimatedDocumentCount();
+            res.send({ count })
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
+    } catch (error) {
+        console.error("Error while connecting to MongoDB:", error);
     }
 }
 run().catch(console.dir);
 
-
 app.get('/', (req, res) => {
-    res.send('furniflex  server is running')
+    res.send('FurniFlex server is running');
 });
 
 app.listen(port, () => {
-    console.log(`furniflex is running port on ${port}`)
-})
+    console.log(`FurniFlex is running on port ${port}`);
+});
